c1ccace50e9bd837dbb841064a037be975b2c476
Code Slayer <3613@holbertonschool.com>
2022-05-02 14:13:32 -0500

    revised

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,17 +5,7 @@ export default function getSanFranciscoDescription() {
       gdp: '$154.2 billion',
       capita: '$178,479',
     };
-    const template = {
-        temp1: 'As of ',
-        temp2: ', it was the seventh-highest income county in the United States',
-        temp3: ', with a per capita personal income of ',
-        temp4: '. As of 2015, San Francisco',
-        temp5: ' proper had a GDP of ',
-        temp6: ', and a GDP per capita of ',
-        period: '.'
-      };
-  
-    return template.temp1 + year + template.temp2
-          / template.temp3 + budget.income + template.temp4
-          / template.temp5 + budget.gdp + template.temp6 + budget.capita + template.period;
+      return `As of ${year}, it was the seventh-highest income county in the United States'
+      / ', with a per capita personal income of ${budget.income}. As of 2015, San Francisco'
+      / ' proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
   }
