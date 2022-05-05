a60f254c3eef5a8efc653141bdc66d88d6433040
Code Slayer <3613@holbertonschool.com>
2022-05-05 17:08:22 -0500

    forgot to return lol

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,8 +5,8 @@ export default function handleProfileSignup(){
     const prom1 = uploadPhoto()
     const prom2 = createUser()
 
-    Promise.all([prom1, prom2]).then((prom1) => {
-        console.log(`${prom1[0].body} ${prom1[1].firstName} ${prom1[1].lastName}`);
+    return Promise.all([prom1, prom2]).then((prom1) => {
+        console.log(prom1[0].body, prom1[1].firstName, prom1[1].lastName);
     })
     .catch(() => {
         console.log("Signup system offline")
