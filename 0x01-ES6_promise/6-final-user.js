24f10e19c2eebb39c2abc205f8b0a2f85afaa8d5
Code Slayer <3613@holbertonschool.com>
2022-05-05 17:29:19 -0500

    return valule added

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,15 +4,15 @@ import uploadPhoto from './5-photo-reject'
 export default async function handleProfileSignup(firstName, lastName, fileName) {
 
 
-    await signUpUser(firstName, lastName).then((prom) => (
+    const prom1 = await signUpUser(firstName, lastName).then((prom) => (
         {'status': 'resolved',
         'vallue': prom,}
     ))
 
-    await uploadPhoto(fileName).catch((prom) => (
+    const prom2 = await uploadPhoto(fileName).catch((prom) => (
         {'status': 'rejected',
-        'value': prom}
+        'value': prom.toString()}
 
     ))
-
+    return Promise.allSettled([prom1, prom2])
 }
