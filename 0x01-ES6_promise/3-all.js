0c36a2f994af4d4ef8dfb95779056811f92a4b2f
Code Slayer <3613@holbertonschool.com>
2022-05-05 17:05:35 -0500

    string syntax

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(){
     const prom2 = createUser()
 
     Promise.all([prom1, prom2]).then((prom1) => {
-        console.log(prom1[0].body, prom1[1].firstName, prom1[1].lastName);
+        console.log(`${prom1[0].body} ${prom1[1].firstName} ${prom1[1].lastName}`);
     })
     .catch(() => {
         console.log("Signup system offline")
