bd4e8498e4c7dcc6df856fc3a69c68f01bd59124
Code Slayer <3613@holbertonschool.com>
2022-05-11 09:45:42 -0500

    error message

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
-  int8[position] = value;
+  try {
+    int8[position] = value;
+  } catch (err) {
+    throw new Error('Position outside range');
+  }
   const dataView = new DataView(buffer);
   return dataView;
 }
