d9c67076b7a5c8eaa185e8503d56c1ccc163a370
Code Slayer <3613@holbertonschool.com>
2022-09-14 08:58:02 -0500

    task 6

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 """task 5 task 5 task 5 task 5 task 5"""
+from xmlrpc.client import boolean
 import bcrypt
-def hash_password(password:str) -> bytes:
+
+def hash_password(password: str) -> bytes:
     """returns salted, hashed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password:bytes, password: str) -> bool:
+    if (bcrypt.hashpw(password, hashed_password) == hashed_password):
+        return True
+    else:
+        return False
