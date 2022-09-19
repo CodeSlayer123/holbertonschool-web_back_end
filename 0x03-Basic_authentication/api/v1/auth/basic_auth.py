00770ea96bac067dc2fae695fa3580f1da7750bf
Code Slayer <3613@holbertonschool.com>
2022-09-19 14:31:52 -0500

    task 10

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ from flask import request
 from typing import TypeVar, List, Tuple
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+
 
 class BasicAuth(Auth):
         """Class BasicAuth inside of file basic_auth"""
@@ -40,3 +42,18 @@ class BasicAuth(Auth):
             if ":" not in decoded_base64_authorization_header:
                 return (None, None)
             return decoded_base64_authorization_header.split(":")
+
+        def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            """returns User instance based on email and password"""
+            if user_email is None or not isinstance(user_email, str):
+                return None
+            if user_pwd is None or not isinstance(user_pwd, str):
+                return None
+            try:
+                for user in User.search({"email": user_email}):
+                    if user.is_valid_password(user_pwd):
+                        return user
+                    else:
+                        return None
+            except:
+                return None
