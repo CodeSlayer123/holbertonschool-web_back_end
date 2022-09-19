64ce7875f730cca0693d74ef43560975afdf7367
Code Slayer <3613@holbertonschool.com>
2022-09-19 11:16:19 -0500

    task 7

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,5 +5,16 @@ from typing import TypeVar, List
 from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
-        """class BasicAuth inside of file basic_auth"""
-
+        """Class BasicAuth inside of file basic_auth"""
+        
+        def extract_base64_authorization_header(self, authorization_header: str) -> str:
+            """returns Base64 part of Authorization header for Basic Authentication"""
+            if authorization_header is None:
+                return None
+            if not isinstance(authorization_header, str):
+                return None
+            split_header = authorization_header.split(" ")
+            if split_header[0] == "Basic":
+                return split_header[1]
+            else:
+                return None
