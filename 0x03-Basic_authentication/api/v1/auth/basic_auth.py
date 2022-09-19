bf9a71fb8dca7e3b588307297fd8e5b4fc2881be
Code Slayer <3613@holbertonschool.com>
2022-09-19 11:06:43 -0500

    task 6

---
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""File basic_auth that contains class auth"""
+from flask import request
+from typing import TypeVar, List
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+        """class BasicAuth inside of file basic_auth"""
+
