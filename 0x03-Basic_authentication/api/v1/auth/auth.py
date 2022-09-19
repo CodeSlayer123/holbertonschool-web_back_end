f9fd1538d8645f5441aa1c6c3332eeba3cf6704c
Code Slayer <3613@holbertonschool.com>
2022-09-19 10:06:23 -0500

    taask 4

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,13 @@ class Auth():
         """class Auth inside of file Auth"""
         def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
             """public method require_auth"""
-            return False
+            if (path is None):
+                return True
+            if (excluded_paths is None or len(excluded_paths) == 0):
+                return True
+            if (path in excluded_paths or (path + "/") in excluded_paths):
+                return False
+            return True
 
         def authorization_header(self, request=None) -> str:
             """public method authorization_header"""
