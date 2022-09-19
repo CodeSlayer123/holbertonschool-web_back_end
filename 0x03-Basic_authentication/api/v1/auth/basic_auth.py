d525a6c97ff606ba4c5836eeaabe96bf7171bd39
Code Slayer <3613@holbertonschool.com>
2022-09-19 11:31:02 -0500

    task 8

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,11 @@
 from flask import request
 from typing import TypeVar, List
 from api.v1.auth.auth import Auth
+import base64
 
 class BasicAuth(Auth):
         """Class BasicAuth inside of file basic_auth"""
-        
+
         def extract_base64_authorization_header(self, authorization_header: str) -> str:
             """returns Base64 part of Authorization header for Basic Authentication"""
             if authorization_header is None:
@@ -17,4 +18,15 @@ class BasicAuth(Auth):
             if split_header[0] == "Basic":
                 return split_header[1]
             else:
+                return None
+
+        def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+            """returns decoded value of a Base64 string"""
+            if base64_authorization_header is None:
+                return None
+            if not isinstance(base64_authorization_header, str):
+                return None
+            try:
+                return base64.b64decode(base64_authorization_header).decode('utf-8')
+            except Exception:
                 return None
