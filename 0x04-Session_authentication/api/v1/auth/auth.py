bcdeb0fc68b2b534492b69d9ac57afb73277af9b
Code Slayer <3613@holbertonschool.com>
2022-09-21 08:55:28 -0500

    pep8 2

--- 0x04-Session_authentication/api/v1/auth/auth.py
+++ 0x04-Session_authentication/api/v1/auth/auth.py
@@ -6,31 +6,31 @@ import os
 
 
 class Auth():
-        """class Auth inside of file Auth"""
-        def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-            """public method require_auth"""
-            if (path is None):
-                return True
-            if (excluded_paths is None or len(excluded_paths) == 0):
-                return True
-            if (path in excluded_paths or (path + "/") in excluded_paths):
-                return False
+    """class Auth inside of file Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method require_auth"""
+        if (path is None):
             return True
+        if (excluded_paths is None or len(excluded_paths) == 0):
+            return True
+        if (path in excluded_paths or (path + "/") in excluded_paths):
+            return False
+        return True
 
-        def authorization_header(self, request=None) -> str:
-            """public method authorization_header"""
-            if request is None:
-                return None
-            if "Authorization" in request.headers:
-                return request.headers["Authorization"]
+    def authorization_header(self, request=None) -> str:
+        """public method authorization_header"""
+        if request is None:
             return None
+        if "Authorization" in request.headers:
+            return request.headers["Authorization"]
+        return None
 
-        def current_user(self, request=None) -> TypeVar('User'):
-            """public method current_user"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method current_user"""
+        return None
+
+    def session_cookie(self, request=None):
+        """returns cookie value from request"""
+        if request is None:
             return None
-
-        def session_cookie(self, request=None):
-            """returns cookie value from request"""
-            if request is None:
-                return None
-            return request.cookies.get(os.getenv("SESSION_NAME"))
+        return request.cookies.get(os.getenv("SESSION_NAME"))
--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -9,38 +9,38 @@ from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
-        """class SessionAuth inside of file session_auth"""
-        user_id_by_session_id = {}
+    """class SessionAuth inside of file session_auth"""
+    user_id_by_session_id = {}
 
-        def create_session(self, user_id: str = None) -> str:
-            """creates Session ID for user_id"""
-            if user_id is None or not isinstance(user_id, str):
-                None
-            session_id = str(uuid.uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
+    def create_session(self, user_id: str = None) -> str:
+        """creates Session ID for user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
-        def user_id_for_session_id(self, session_id: str = None) -> str:
-            """returns User ID based on Session ID"""
-            if session_id is None or not isinstance(session_id, str):
-                None
-            return self.user_id_by_session_id.get(session_id)
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns User ID based on Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            None
+        return self.user_id_by_session_id.get(session_id)
 
-        def current_user(self, request=None):
-            """returns User instance based on cookie value"""
-            return User.get(
-                self.user_id_for_session_id(
-                    self.session_cookie(request)))
+    def current_user(self, request=None):
+        """returns User instance based on cookie value"""
+        return User.get(
+            self.user_id_for_session_id(
+                self.session_cookie(request)))
 
-        def destroy_session(self, request=None):
-            """deletes user session / logout"""
-            if request is None:
-                return False
-            session_id = self.session_cookie(request)
-            if session_id is None:
-                return False
-            user_id = self.user_id_for_session_id(session_id)
-            if user_id is None:
-                return False
-            del self.user_id_by_session_id[session_id]
-            return True
+    def destroy_session(self, request=None):
+        """deletes user session / logout"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
