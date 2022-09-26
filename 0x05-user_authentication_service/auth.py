b147c27aeea19b45823b06b358311f65308dcd14
Code Slayer <3613@holbertonschool.com>
2022-09-26 15:23:01 -0500

    task 10

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -53,7 +53,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
-            return uuid
+            return session_id
         except Exception:
             return None
 
