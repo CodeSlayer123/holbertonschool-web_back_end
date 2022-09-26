afee3df702c505f8dd49be4d0432d7fe2272a909
Code Slayer <3613@holbertonschool.com>
2022-09-26 15:41:14 -0500

    removed is staement

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -59,8 +59,7 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """returns user from given session_id"""
-        if session_id is None:
-            return None
+
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
