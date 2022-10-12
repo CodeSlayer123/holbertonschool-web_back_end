b5386e08a7b88a581455be3de3f082064330ae03
Code Slayer <3613@holbertonschool.com>
2022-10-12 09:19:19 -0500

    teset 3

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- this is task 0
 -- creates table users
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
