28ecf63a1c524a5efebd7878a93bf633761f5201
Code Slayer <3613@holbertonschool.com>
2022-10-12 09:31:38 -0500

    user to users

--- 0x01-MySQL_Advanced/1-country_users.sql
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- this is task 1
 -- creates table users
-CREATE TABLE IF NOT EXISTS user (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM ('US','CO','TN') NOT NULL DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US','CO','TN') NOT NULL DEFAULT 'US'
 );
