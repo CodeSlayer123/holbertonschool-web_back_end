bbe60462367838e12ece5c9196f432baef668220
Code Slayer <3613@holbertonschool.com>
2022-10-25 07:59:55 -0500

    lint again

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -9,21 +9,19 @@ const countStudents = async (path) => {
   fs.access(path, (error) => {
     if (error) {
       throw new Error('Cannot load the database');
-    }})
-
-  const newPromise = await fs.promises.readFile(path,'utf8');
-
-  let data = newPromise.split('\n');
-
+    }
+  })
+  const newPromise = await fs.promises.readFile(path, 'utf8');
+  const data = newPromise.split('\n');
   for (let i = 1; i < data.length; i += 1){
 
-    if (data[i].length > 0){
+    if (data[i].length > 0) {
       students.push(data[i].split(','));
     }
   }
 
   for (const i in students) {
-    if (students[i][3] === 'CS'){
+    if (students[i][3] === 'CS') {
       cs.push(students[i][0]);
     } else if (students[i][3] === 'SWE') {
       swe.push(students[i][0]);
@@ -36,6 +34,5 @@ const countStudents = async (path) => {
   const result = `${numOfStudents}\n${numOfStudentsCS}\n${numOfStudentsSWE}`;
   console.log(result);
   return result;
-}
-
+};
 module.exports = countStudents;
