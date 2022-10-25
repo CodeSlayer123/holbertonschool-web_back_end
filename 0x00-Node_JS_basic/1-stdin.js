8b25ac5c25852156c8188dc28a5d39ccd252e19b
Code Slayer <3613@holbertonschool.com>
2022-10-25 07:22:30 -0500

    task 1 reworked

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,29 +1,10 @@
-var fs=require("fs")
-function pipedIn(cb) {
-    fs.fstat(0, function(err, stats) {
-        cb(null, stats.isFIFO())
-    })
-}
+console.log('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+    let name = process.stdin.read();
+    if (name) {
+        process.stdout.write(`Your name is: ${name}`);
+    }
 
-function pipedOut(cb) {
-    fs.fstat(1, function(err, stats) {
-        cb(null, stats.isFIFO())
-    })
-}
-
-
-const { resolve } = require('path');
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  readline.question('Welcome to Holberton School, what is your name?\n', name => {
-      console.log(`Your name is: ${name}`);
-      readline.close();
-  })
-  readline.on('close', () => {
-        pipedIn((err, x) => (x === true) ? console.log("This important software is now closing") : 'invalid')
-
-  });
-
+}).on('end', () => {
+    process.stdout.write('This important software is now closing');
+});
