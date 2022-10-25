f5f7f6425f36b234b7ff431fc07b574b1e7c4e43
Code Slayer <3613@holbertonschool.com>
2022-10-25 07:35:44 -0500

    lint

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -5,7 +5,6 @@ process.stdin.on('readable', () => {
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   }
-
 }).on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -1,35 +1,34 @@
-var fs=require("fs")
+var fs=require("fs");
 
 function countStudents(path){
-    students = []
-    cs = []
-    swe = []
-    if (!fs.existsSync(path)) {
-        throw new Error("Cannot load the database");
+  students = [];
+  cs = [];
+  swe = [];
+  if (!fs.existsSync(path)) {
+    throw new Error("Cannot load the database");
+  }
+
+  const data = fs.readFileSync(path, 'utf8').split("\n");
+
+  for (let i = 1; i < data.length; i++){
+    if (data[i].length > 0){
+      students.push(data[i].split(","));
     }
+  }
 
-    const data = fs.readFileSync(path, 'utf8').split("\n");
-
-    for (let i = 1; i < data.length; i++){
-
-        if (data[i].length > 0){
-            students.push(data[i].split(","))
-        }
+  for (i in students){
+    if (students[i][3] === 'CS'){
+      cs.push(students[i][0]);
     }
-
-    for (i in students){
-        if (students[i][3] === 'CS'){
-            cs.push(students[i][0])
-        }
-        else if (students[i][3] === 'SWE') {
-            swe.push(students[i][0])
-        }
+    else if (students[i][3] === 'SWE') {
+      swe.push(students[i][0]);
     }
+  }
 
-  console.log(`Number of students: ${students.length}`)
-  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(", ")}`)
-  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(", ")}`)
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(", ")}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(", ")}`);
 
 }
 
-module.exports = countStudents
+module.exports = countStudents;
